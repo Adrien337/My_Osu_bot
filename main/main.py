d338358d8cfdd45102fdecb6a4b3b246e8b71f63
Adrien337 <adrien.cros2007@gmail.com>
2025-12-15 20:23:09 +0100

    Fixed getMapFolder & added getMapFile

--- main/main.py
+++ main/main.py
@@ -6,7 +6,7 @@ import win32gui
 
 ##### Path to Osu! map folder
 
-userName = "" # Put your user name here
+userName = "adrie" # Put your user name here
 pathToSongs = fr"C:\Users\{userName}\AppData\Local\osu!\Songs"
 
 
@@ -17,41 +17,57 @@ def getMapName():
         currentWindow = win32gui.GetWindowText(win32gui.GetForegroundWindow())
         if not currentWindow: # If there's no window detected
             print("No active window detected")
-            return -1
+            return -1, -1, -1
         if "osu!" not in currentWindow.lower(): # If Osu! isn't focused
-            print("Focus the osu! window")
-            return -1
+            print("Focus on the osu! window")
+            return -1, -1, -1
         if currentWindow == "osu!": # If we did not choose a beatmap
             print("Choose a beatmap")
-            return -1
+            return -1, -1, -1
         
         mapNameInformations = currentWindow.split(" - ") # We get rid of the "osu!" in the window name
         mapArtist = mapNameInformations[1] # We take the artist name
-        mapName = mapNameInformations[2].split("[")[0] # We take the beatmap name after getting rid of the beatmap creator
+        mapNameInformations = mapNameInformations[2].split("[") # We split the difficulty and beatmap name
+        mapName = mapNameInformations[0][0:-1:1] # We take the map name (and remove the extra space)
+        mapDifficulty = mapNameInformations[-1][0:-1:1] # We take the map difficulty (and remove the extra space)
 
-        return mapArtist, mapName
+        print(f"Beatmap found !\n"
+              f"Artist : {mapArtist}\n"
+              f"Title : {mapName}\n"
+              f"Difficulty : {mapDifficulty}")
+        return mapArtist, mapName, mapDifficulty
     except:
         print("Something went wrong. Please try again") # Please tell me that this will never appear
         return -1
 
-def getMapInfos(artist, title): # Don't work properly. I think I know the reason it don't work.
+def getMapFolder(artist, title): # Look for the right beatmap in the Songs folder
     # Get all folders inside Songs
     folders = [f for f in os.listdir(pathToSongs) if os.path.isdir(os.path.join(pathToSongs, f))]
 
-    for folder in folders:
+    for folder in folders: # We list all folders inside the Songs folder
         if (artist.lower() in folder.lower()) and (title.lower() in folder.lower()):
-            folder_path = os.path.join(pathToSongs, folder)
-            print(f"Beatmap found! The folder is {folder_path}")
-            return folder_path
+            folderPath = os.path.join(pathToSongs, folder)
+            print(f"Beatmap folder found !")
+            return folderPath
 
-    print("Beatmap not found. Please check your folders")
+    print("Beatmap folder not found. Please check your Song folder")
+    return -1
+
+def getMapFile(folderPath, title, difficulty):
+    # Get all files inside the song folder
+    for file in os.listdir(folderPath): # We check all files inside the song folder
+        if (title.lower() in file.lower()) and (difficulty.lower() in file.lower()):
+            print("Beatmap file found !")
+            return file
+    print("Beatmap file not found. Please check your folders")
     return -1
 
 ##### Main code
 
 keyboard.wait("x")
-artist, title = getMapName()
-print(f"Artist : {artist}")
-print(f"Title : {title}")
-
-getMapInfos(artist, title)
+artist, title, difficulty = getMapName()
+if not -1 in [artist, title, difficulty]:
+    folderPath = getMapFolder(artist, title)
+    MapInformations = getMapFile(folderPath, title, difficulty)
+    if MapInformations != -1:
+        print(MapInformations)
